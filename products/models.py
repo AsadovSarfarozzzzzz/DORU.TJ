af3572ac1164bbabac45a58289f2830aca781748
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-25 18:50:20 +0500

    commit three

--- products/models.py
+++ products/models.py
@@ -8,3 +8,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
