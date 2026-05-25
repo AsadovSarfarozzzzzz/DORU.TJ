6a056380bacf84c37f1e7229f7642114ac61cf33
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-25 19:06:25 +0500

    commit 11

--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
     
+class EmailConfirm(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)  # когда создан
+    is_confirmed = models.BooleanField(default=False)    
+
+    def __str__(self):
+        return self.user.username
