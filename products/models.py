d827bef2e6ab82b317f61f439ca682808cd6c8c6
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-25 18:52:13 +0500

    commit four

--- products/models.py
+++ products/models.py
@@ -16,3 +16,18 @@ class Manufacturer(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    is_prescription = models.BooleanField(default=False)
+    active_substance = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
