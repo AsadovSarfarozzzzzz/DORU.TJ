393a9288b8306f8d44c24255a0cc2061739d3769
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-26 19:52:46 +0500

    commit 21 views/accounts

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
     address = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.username
