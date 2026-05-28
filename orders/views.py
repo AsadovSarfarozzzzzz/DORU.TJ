acd95e41e8a7a0e674fdbe57f590da183fc1e4c0
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-28 06:31:41 +0500

    views/orders 5

--- orders/views.py
+++ orders/views.py
@@ -84,3 +84,10 @@ def checkout(request):
         'items': items,
         'total': total
     })
+
+@login_required
+def my_orders(request):
+    orders = Order.objects.filter(
+        user=request.user
+    ).order_by('-created_at')
+    return render(request, 'orders/my_orders.html', {'orders': orders})
