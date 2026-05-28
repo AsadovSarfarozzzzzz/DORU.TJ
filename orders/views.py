23363e988fc3a89571f0a675e487e10dbca390c4
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-28 06:30:14 +0500

    views/orders 1

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Cart, CartItem, Order, OrderItem
+from products.models import Product
 
-# Create your views here.
+@login_required
+def add_to_cart(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+    item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        item.quantity += 1
+        item.save()
+    messages.success(request, f'{product.name} добавлен в корзину!')
+    return redirect(request.META.get('HTTP_REFERER', 'catalog'))
