37e39e5e62858bb2f812ce7be5aa379f6028bf6d
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-28 06:28:06 +0500

    views/products 1

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category, Manufacturer
 
-# Create your views here.
+def home(request):
+    categories = Category.objects.all()
+    popular = Product.objects.filter(stock__gt=0).order_by('-id')[:8]
+    return render(request, 'products/home.html', {
+        'categories': categories,
+        'popular': popular
+    })
