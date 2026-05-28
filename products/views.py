d0f2fea4725cf511d2b4cf165d18e8ecbdf740fb
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-28 06:28:33 +0500

    views/products 2

--- products/views.py
+++ products/views.py
@@ -8,3 +8,32 @@ def home(request):
         'categories': categories,
         'popular': popular
     })
+
+
+def catalog(request):
+    products = Product.objects.filter(stock__gt=0)
+    categories = Category.objects.all()
+    
+    # фильтры
+    category_slug = request.GET.get('category')
+    search = request.GET.get('search')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    is_prescription = request.GET.get('is_prescription')
+
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+    if search:
+        products = products.filter(name__icontains=search) | \
+                   products.filter(active_substance__icontains=search)
+    if min_price:
+        products = products.filter(price__gte=min_price)
+    if max_price:
+        products = products.filter(price__lte=max_price)
+    if is_prescription:
+        products = products.filter(is_prescription=True)
+
+    return render(request, 'products/catalog.html', {
+        'products': products,
+        'categories': categories,
+    })
