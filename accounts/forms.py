48aebc00727c9b9dcc28646d02371899e25edcf9
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-30 13:24:42 +0500

    this is validation proverka

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,23 @@ class RegisterForm(forms.Form):
     phone = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder' : '+992 XX XXX XXXX'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Enter your password again'}))
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Логин уже занят')
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email уже используется')
+        return email
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cleaned_data
