7772045632f1e22353d9821cbd6a3d9cf5f9b7cc
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-30 22:52:16 +0500

    add to urls

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('catalog/', views.catalog, name='catalog'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product_list/', views.product_list, name='product_list'),
 ]
