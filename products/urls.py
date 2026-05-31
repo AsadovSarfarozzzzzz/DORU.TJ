3e2012f9d8b25ec8abcad82e36ab69d5345dbd06
Sarfaroz <adolfhitler1637@gmail.com>
2026-05-31 11:03:11 +0500

    update urls

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('catalog/', views.catalog, name='catalog'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('product_list/', views.product_list, name='product_list'),
-    path('product_delete/', views.product_delete, name='product_delete'),
-    path('product_update/', views.update_product, name='product_update'),
-    path('product_add', views.product_add, name='product_add'),
+    path('admin-panel/products/', views.product_list, name='product_list'),
+    path('admin-panel/products/add/', views.product_add, name='product_add'),
+    path('admin-panel/products/edit/<int:pk>/', views.product_edit, name='product_edit'),
+    path('admin-panel/products/delete/<int:pk>/', views.product_delete, name='product_delete'),
 ]
