b6407cec46d619e407100c29a1a0ed4491e2e804
Sarfaroz <adolfhitler1637@gmail.com>
2026-06-02 20:59:58 +0500

    pharmacy views.py

--- pharmacies/views.py
+++ pharmacies/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from .models import Pharmacy, PharmacyProduct
+from products.models import Product
 
-# Create your views here.
+
+class PharmacyListView(generic.ListView):
+    model = Pharmacy
+    template_name = 'pharmacy_list.html'
+    context_object_name = 'pharmacies'
+
+    def get_queryset(self):
+        return Pharmacy.objects.filter(is_active=True)
+
+
+class PharmacyDetailView(generic.DetailView):
+    model = Pharmacy
+    template_name = 'pharmacy_detail.html'
+    context_object_name = 'pharmacy'
+
+    def get_queryset(self):
+        return Pharmacy.objects.filter(is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = PharmacyProduct.objects.filter(
+            pharmacy=self.object
+        ).select_related('product')
+        return context
+
+
+class ProductPharmaciesView(generic.DetailView):
+    model = Product
+    template_name = 'product_pharmacies.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pharmacies'] = PharmacyProduct.objects.filter(
+            product=self.object,
+            pharmacy__is_active=True
+        ).select_related('pharmacy').order_by('price')
+        return context
