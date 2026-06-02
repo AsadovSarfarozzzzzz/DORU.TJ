e8a39a33a1535d7a12597c18b1740bb9115afa49
Sarfaroz <adolfhitler1637@gmail.com>
2026-06-02 20:56:08 +0500

    add model pharmacy

--- pharmacies/models.py
+++ pharmacies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    working_hours = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
