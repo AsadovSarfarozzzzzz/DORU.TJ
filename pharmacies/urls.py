08e13212d66149b11c024aa6dbdc6d5fbaffb450
Sarfaroz <adolfhitler1637@gmail.com>
2026-06-03 14:12:18 +0500

    update urls.py

--- pharmacies/urls.py
+++ pharmacies/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.PharmacyListView.as_view(), name='pharmacy_list'),
     path('<int:pk>/', views.PharmacyDetailView.as_view(), name='pharmacy_detail'),
     path('product/<int:pk>/', views.ProductPharmaciesView.as_view(), name='product_pharmacies'),
+    path('<int:pk>/chat/', views.pharmacy_chat, name='pharmacy_chat'),
+    path('<int:pk>/chat/api/', views.pharmacy_chat_api, name='pharmacy_chat_api'),
+    path('manager/<int:pk>/', views.manager_panel, name='manager_panel'),
+    path('manager/chat/<int:chat_pk>/', views.manager_chat, name='manager_chat'),
 ]
